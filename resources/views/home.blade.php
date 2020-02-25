b1e4dc460b70e9a3919dbac7c12ac8c16df77e06
utpal21 <utpal.uoda@gmail.com>
2020-02-25 16:59:58 +0600

    add demo products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <!-- <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
@@ -16,7 +16,7 @@
 
                     You are logged in!
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+Route::resource('products','ProductController');
+
